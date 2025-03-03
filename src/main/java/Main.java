1a39c39c034469115811ad577329be7ff9b0e300
shivam <shivam_0217@rediffmail.com>
2025-03-04 00:14:02 +0530

    added listener to return 200ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,11 @@ public class Main {
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
+       Socket clientSocket = serverSocket.accept();
+
+       clientSocket.getOutputStream().write(
+               "HTTP/1.1 200 OK\r\n\r\n".getBytes()
+       );
 
        serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
